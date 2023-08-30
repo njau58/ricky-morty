feffcb4aa681a8709a644b4ad9c6413287072e6a
njauprince@gmail.com <njauprince@gmail.com>
2023-08-30 13:28:33 +0300

    final client fix

--- src/components/Image/ImageCustom.jsx
+++ src/components/Image/ImageCustom.jsx
@@ -1,13 +1,28 @@
 import Image from "next/image";
+import ProjectSkeleton from "../skeleton/ProjectSkeleton";
+import { useState } from "react";
 
 const ImageCustom = ({ src, alt }) => {
+  const [isImageReady, setIsImageReady] = useState(false);
+  const onLoadCallBack = (img) => {
+    setIsImageReady(true);
+    console.log(img);
+    typeof onLoadingComplete === "function" && onLoadingComplete(e);
+  };
+
   return (
     <>
-      <Image
+      {isImageReady ? (
+        <Image
         src={src}
+        onLoadingComplete={(img) => onLoadCallBack(img)}
         alt={alt}
         className="rounded-md w-full h-full object-cover"
       ></Image>
+      ) : (
+      
+        <ProjectSkeleton />
+      )}
     </>
   );
 };
